206fdffedfae2752e3f8f6aaf229acc3e08f8bb4
Tim Corley <tcorley215@gmail.com>
2020-07-10 13:15:13 -0400

    creating Core dir, adding variable notes

---
+++ Core/variables.js
@@ -0,0 +1,28 @@
+// undeclared
+var a
+
+console.log(typeof a);
+
+// declare multiple variables once in same statement
+var a = 1, b = 2*2
+
+console.log(a);
+console.log(b);
+
+let c = 'foo'
+let d = 'bar'
+
+console.log(c);
+console.log(d);
+
+const e = ['alpha', 'bravo']
+
+console.log(e);
+
+
+
+
+
+
+
+
