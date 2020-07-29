b2ebac6cf1b7e8f8e865888db6b84ce5e29c734f
Tim Corley <tcorley215@gmail.com>
2020-07-29 15:23:13 -0400

    updating babel & adding tailwind - mvp complete

--- React/scratch-starter-app/webpack.config.js
+++ React/scratch-starter-app/webpack.config.js
@@ -1,9 +1,5 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-
-const htmlWebpackPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-  filename: "./index.html"
-});
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   module: {
@@ -12,10 +8,26 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: "babel-loader"
         }
-      }
+      },
+       {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader", "postcss-loader",
+          ],
+      },
     ]
   },
-  plugins: [htmlWebpackPlugin]
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+      chunkFilename: "styles.css"
+    }),
+    new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+  }),
+]
 };
